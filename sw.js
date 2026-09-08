c14bc60b2a0371081d67a139fd9eb7b0879d261f
grisonb <79510016+grisonb@users.noreply.github.com>
2026-09-08 15:26:22 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const SW_VERSION = 'sw-v16-58_freq_all_zoom_stable';
-const APP_VERSION = 'v16.58';
+const SW_VERSION = 'sw-v16-59_glr_fuel_zoom_serial';
+const APP_VERSION = 'v16.59';
 const SIA_DATA_REVISION = '15.69';
 const SIA_DATA_URL = './sia.js';
 const SIA_DATA_CACHE = `npf-q400-sia-data-${SIA_DATA_REVISION}`;
