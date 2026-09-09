713124d9c761da25d25f2b8ca05e7b50db85b21c
grisonb <79510016+grisonb@users.noreply.github.com>
2026-09-09 11:50:34 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const SW_VERSION = 'sw-v16-62_glr_3probe_heavyzoom_360_source_release';
-const APP_VERSION = 'v16.62';
+const SW_VERSION = 'sw-v16-63_bfg_no_password_airport_services_sia_labels_yul_trace';
+const APP_VERSION = 'v16.63';
 const SIA_DATA_REVISION = '15.69';
 const SIA_DATA_URL = './sia.js';
 const SIA_DATA_CACHE = `npf-q400-sia-data-${SIA_DATA_REVISION}`;
