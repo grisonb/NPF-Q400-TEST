4e76c267e508211ff23fa1629d9fc867acf06909
grisonb <79510016+grisonb@users.noreply.github.com>
2026-09-10 17:06:43 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const SW_VERSION = 'sw-v16-65_trace_state_airport_833_sia_inner_bands';
-const APP_VERSION = 'v16.65';
+const SW_VERSION = 'sw-v16-66_airport_services_2nm_sia_idle_alias_perf';
+const APP_VERSION = 'v16.66';
 const SIA_DATA_REVISION = '15.69';
 const SIA_DATA_URL = './sia.js';
 const SIA_DATA_CACHE = `npf-q400-sia-data-${SIA_DATA_REVISION}`;
