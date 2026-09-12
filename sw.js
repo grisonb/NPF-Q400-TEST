91ca11ad827920ca9bdff1fcd783fbdcafb1260b
grisonb <79510016+grisonb@users.noreply.github.com>
2026-09-12 19:33:09 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const SW_VERSION = 'sw-v16-74_sia_full_guard';
-const APP_VERSION = 'v16.74';
+const SW_VERSION = 'sw-v16-75_departments_lazy';
+const APP_VERSION = 'v16.75';
 const SIA_DATA_REVISION = '15.69-radio1026-1-maplite1';
 const SIA_DATA_URL = './sia.js';
 const SIA_DATA_CACHE = `npf-q400-sia-data-${SIA_DATA_REVISION}`;
