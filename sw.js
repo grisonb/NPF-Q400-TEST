5572192aeda08da7662e9c28bc15d0ca5a48e62e
grisonb <79510016+grisonb@users.noreply.github.com>
2026-09-12 20:22:48 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const SW_VERSION = 'sw-v16-76_routes_tier0_vfr_colors';
-const APP_VERSION = 'v16.76';
+const SW_VERSION = 'sw-v16-77_routes_compact_workset';
+const APP_VERSION = 'v16.77';
 const SIA_DATA_REVISION = '15.69-radio1026-1-maplite1';
 const SIA_DATA_URL = './sia.js';
 const SIA_DATA_CACHE = `npf-q400-sia-data-${SIA_DATA_REVISION}`;
