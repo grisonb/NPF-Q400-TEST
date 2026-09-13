2da480983f0ad9d6be2ff46d6f3b701cd11b5c7d
grisonb <79510016+grisonb@users.noreply.github.com>
2026-09-13 11:08:56 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const SW_VERSION = 'sw-v16-80_routes_warm_tier0_short_wait';
-const APP_VERSION = 'v16.80';
+const SW_VERSION = 'sw-v16-81_routes_no_final_tile_wait';
+const APP_VERSION = 'v16.81';
 const SIA_DATA_REVISION = '15.69-radio1026-1-maplite1';
 const SIA_DATA_URL = './sia.js';
 const SIA_DATA_CACHE = `npf-q400-sia-data-${SIA_DATA_REVISION}`;
