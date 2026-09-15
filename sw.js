3ce733e97e9c48174e70d74c138445d9aa6c4cff
grisonb <79510016+grisonb@users.noreply.github.com>
2026-09-15 19:33:22 +0200

    Update service worker version and app version

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const SW_VERSION = 'sw-v16-89_pan_jank_diagnostic';
-const APP_VERSION = 'v16.89';
+const SW_VERSION = 'sw-v16-90_airport_labels_pan_stability';
+const APP_VERSION = 'v16.90';
 const SIA_DATA_REVISION = '15.69-radio1026-1-maplite1';
 const SIA_DATA_URL = './sia.js';
 const SIA_DATA_CACHE = `npf-q400-sia-data-${SIA_DATA_REVISION}`;
