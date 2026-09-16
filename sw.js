d953a63c28d6270449cec47c32142e5720504f1f
grisonb <79510016+grisonb@users.noreply.github.com>
2026-09-16 09:05:58 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const SW_VERSION = 'sw-v16-91_zoomout_tile_priority';
-const APP_VERSION = 'v16.91';
+const SW_VERSION = 'sw-v16-92_tile_reference_passive_diag';
+const APP_VERSION = 'v16.92';
 const SIA_DATA_REVISION = '15.69-radio1026-1-maplite1';
 const SIA_DATA_URL = './sia.js';
 const SIA_DATA_CACHE = `npf-q400-sia-data-${SIA_DATA_REVISION}`;
