37e5bc2c0c925e51ee01c77562143d2a37fd4f94
grisonb <79510016+grisonb@users.noreply.github.com>
2026-09-16 13:34:57 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const SW_VERSION = 'sw-v16-94_wp_current_position_route';
-const APP_VERSION = 'v16.94';
+const SW_VERSION = 'sw-v16-95_wp_fixed_origin_editable_first_leg';
+const APP_VERSION = 'v16.95';
 const SIA_DATA_REVISION = '15.69-radio1026-1-maplite1';
 const SIA_DATA_URL = './sia.js';
 const SIA_DATA_CACHE = `npf-q400-sia-data-${SIA_DATA_REVISION}`;
