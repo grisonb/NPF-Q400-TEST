d08f46a607a9cd4f2d0025f5d7750608230626f5
grisonb <79510016+grisonb@users.noreply.github.com>
2026-09-18 18:47:01 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const SW_VERSION = 'sw-v17-10_renderer_suspend_ht_atomic_swap';
-const APP_VERSION = 'v17.10';
+const SW_VERSION = 'sw-v17-11_touch_premask_no_renderer_detach';
+const APP_VERSION = 'v17.11';
 const SIA_DATA_REVISION = '15.69-radio1026-1-maplite1';
 const SIA_DATA_URL = './sia.js';
 const SIA_DATA_CACHE = `npf-q400-sia-data-${SIA_DATA_REVISION}`;
