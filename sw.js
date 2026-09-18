d074554cc4f3f18b274ae24d25442c8da2fb899e
grisonb <79510016+grisonb@users.noreply.github.com>
2026-09-18 12:47:32 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const SW_VERSION = 'sw-v17-02_overlay_priority_tiles_vfr_ht_routes';
-const APP_VERSION = 'v17.02';
+const SW_VERSION = 'sw-v17-03_overlay_priority_targeted';
+const APP_VERSION = 'v17.03';
 const SIA_DATA_REVISION = '15.69-radio1026-1-maplite1';
 const SIA_DATA_URL = './sia.js';
 const SIA_DATA_CACHE = `npf-q400-sia-data-${SIA_DATA_REVISION}`;
