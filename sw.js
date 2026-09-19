ff5d4bbf875609ecf2e26e5f92479397a47e35a5
grisonb <79510016+grisonb@users.noreply.github.com>
2026-09-19 10:23:10 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const SW_VERSION = 'sw-v17-19_startup_tile_priority';
-const APP_VERSION = 'v17.19';
+const SW_VERSION = 'sw-v17-20_startup_map_gate';
+const APP_VERSION = 'v17.20';
 const SIA_DATA_REVISION = '15.69-radio1026-1-maplite1';
 const SIA_DATA_URL = './sia.js';
 const SIA_DATA_CACHE = `npf-q400-sia-data-${SIA_DATA_REVISION}`;
