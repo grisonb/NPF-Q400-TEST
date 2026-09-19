b5dd0de4eac3dfbf49466146ec2a0a2e2895ff91
grisonb <79510016+grisonb@users.noreply.github.com>
2026-09-19 12:26:49 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const SW_VERSION = 'sw-v17-21_startup_zoomout_return_cache_ht50';
-const APP_VERSION = 'v17.21';
+const SW_VERSION = 'sw-v17-22_ram_fastpath_zoomout_startup_v20_ht50';
+const APP_VERSION = 'v17.22';
 const SIA_DATA_REVISION = '15.69-radio1026-1-maplite1';
 const SIA_DATA_URL = './sia.js';
 const SIA_DATA_CACHE = `npf-q400-sia-data-${SIA_DATA_REVISION}`;
